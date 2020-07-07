bb92af677bd4ce9df79cc88ac99f0d8bccaf3770
Pangseen Puarat <pangseen.puarat@xencio.at>
2020-07-07 17:17:25 +1200

    Added login for assessment takers.

--- src/services/Api.js
+++ src/services/Api.js
@@ -27,10 +27,10 @@ class Api {
         })
     }
 
-    fetchSubscribedPolicy(policyId) {
+    fetchSubscribedPolicy(subscribedPolicyId) {
         return Axios.get("/getSubscribedPolicy", {
             params: {
-                policy_id: policyId
+                subscribedPolicyId: subscribedPolicyId
             }
         })
     }
--- src/views/dashboardAccountablePersonSubPage/Assessment.jsx
+++ src/views/dashboardAccountablePersonSubPage/Assessment.jsx
@@ -9,14 +9,15 @@ import {
   Row,
   Col,
   Input,
+  InputGroup,
   Button,
   Card,
   CardHeader,
   CardBody,
   CardTitle,
-  Label
+  Label,
+  Jumbotron
 } from "reactstrap";
-import { Checkbox } from "antd";
 
 let checkedAnswer;
 let isAnswerCorrect = [];
@@ -28,22 +29,24 @@ class Assessment extends React.Component {
     this.renderAssessment = this.renderAssessment.bind(this);
     this.renderOption = this.renderOption.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.renderLoginPage = this.renderLoginPage.bind(this);
     this.subscribedPolicyId = "";
     this.state = {
+      isLoggedIn: false,
       assessment: [],
       checkedAnswer: "",
       policies: [],
       currentPolicy: [],
+      loginEmail: ""
     };
     this.api = new Api();
-    document.documentElement.classList.remove("nav-open");
   }
 
   componentDidMount() {
     this.subscribedPolicyId = this.props.match.params.subscribedPolicyId;
     this.api.fetchSubscribedPolicy(this.subscribedPolicyId)
       .then(response => {
-        console.log(response)
+        console.log(response.data);
         this.setState({
           currentPolicy: response.data,
           assessment: response.data.assessments
@@ -51,12 +54,13 @@ class Assessment extends React.Component {
       }).catch(function (error) {
         console.log(error);
       });
+
   }
   componentDidUpdate() {
-    document.body.classList.remove("register-page");
   }
 
   onSubmit() {
+    return;
     isAnswerCorrect.forEach(answer => {
       if (answer) {
         score = score + 1;
@@ -83,45 +87,44 @@ class Assessment extends React.Component {
       })
   }
 
+
   renderAssessment(assessment, assessmentIndex) {
     console.log(assessment);
     return (
-      <div>
-        <h4>{assessment.assessment_content}</h4>
-          
-        <ul style={{ listStyleType: "none" }}>
-          {assessment.options.map((option, optionIndex) => (
-            <Row>
-              <Col md="10">
-                <li style={{ listStyleType: "none" }}>
-                  {this.renderOption(option, optionIndex, assessment.correct_answer, assessment.assessment_content)}
-                </li>
-              </Col>
-            </Row>
-          ))}
-        </ul>
-        <br></br>
-        <br></br>
-        <hr></hr>
-      </div>
+      <Row style={{ justifyContent: "center" }}>
+        <Col xs="10">
+          <h4>{assessment.assessment_content}</h4>
+
+          <ul style={{ listStyleType: "none" }}>
+            {assessment.options.map((option, optionIndex) => (
+              <li style={{}}>
+                {this.renderOption(option,
+                  optionIndex,
+                  assessment.correct_answer,
+                  assessment.assessment_content)}
+              </li>
+            ))}
+          </ul>
+        </Col>
+        <div style={{ borderBottom: " 1px solid #969696", width: "90%" }}></div>
+      </Row>
     );
   }
 
   renderOption(option, optionIndex, correct_answer, content) {
     const handleChange = e => {
       checkedAnswer = e.target.value;
-      if (checkedAnswer === correct_answer) {
-        isAnswerCorrect.push(true);
+      if (checkedAnswer == correct_answer) {
+        option.isSelected = true;
+      } else {
+        option.isSelected = false;
       }
-      else {
-        isAnswerCorrect.push(false);
-      }
-      console.log("ASDF" + isAnswerCorrect)
+      console.log(option);
     };
     content = content.replace(/\s+/g, '-').toLowerCase();
     return (
       <div>
-        <Row style={{ marginTop: '12px' }}>
+        <Row style={{ paddingTop: '10px' }}>
           <Col md="8">
             <Label check>
               <Input type="radio" name={content} value={optionIndex + 1} onChange={handleChange} />
@@ -133,42 +136,90 @@ class Assessment extends React.Component {
     );
   }
 
-  render() {
+  renderLoginPage() {
 
-    return (
-      <>
-        <div className="content">
-          <Row>
-            <Col className="ml-auto mr-auto" md="12">
-              <Card className="card-upgrade" style={{ transform: 'none' }}>
-                <CardHeader className="text-center">
-                  <CardTitle tag="h4">Assessment Questions</CardTitle>
-                  <p className="card-category">
-                    Assessment Questions for <b>{this.state.currentPolicy.policy_name}</b>
-                  </p>
-                  <Link to="/dashboard/policies" className="btn btn-danger">
-                    &#8592; Go Back to Policies
-                  </Link>
-                </CardHeader>
-                <CardBody>
-                  <ul style={{ listStyleType: "none" }}>
-                    {this.state.assessment.map((assessment, assessmentIndex) => (
-                      <React.Fragment>
-                        <br></br>
-                        <li style={{ listStyleType: "none" }} key={assessmentIndex}>{this.renderAssessment(assessment, assessmentIndex)}</li>
-                      </React.Fragment>
-                    ))}
-                  </ul>
-                  <Button className="btn-round" color="success" onClick={this.onSubmit}>Submit</Button>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </div>
+    const login = () => {
+      const assessmentTakerList = this.state.currentPolicy.assessment_takers;
+      const isEmailExists = assessmentTakerList.some(taker => taker.user.email === this.state.loginEmail);
+      if (isEmailExists) {
+        this.setState({
+          isLoggedIn: true
+        })
+      } else {
+        alert("Your email is not valid");
+        this.setState({
+          loginEmail: ""
+        })
+      }
+    }
 
-      </>
-    )
+    const handleEmailInput = (event) => {
+      this.setState({
+        loginEmail: event.target.value
+      })
+    }
+    return <>
+      <Row style={{ paddingTop: "2rem" }}>
+        <Col xs="0" md="3"></Col>
+        <Col xs="12" md="6">
+          <Jumbotron>
+            <h1>Please Login with your email to start the assessment</h1>
+            <p className="lead">
+              <InputGroup>
+                <Input placeholder="someone@example.com"
+                  value={this.state.loginEmail}
+                  onChange={handleEmailInput} />
+              </InputGroup>
+            </p>
+            <hr className="my-2" />
+            <p className="lead">
+              <Button color="primary" onClick={login}>Login</Button>
+            </p>
+          </Jumbotron>
+        </Col>
+        <Col xs="0" md="3"></Col>
+      </Row>
+    </>
   }
+
+  render() {
+    // if not logged in, show login screen
+    if (!this.state.isLoggedIn) {
+      return this.renderLoginPage();
+    } else {
+      // If loggedIn show the assessment
+      return <>
+        <Row>
+          <Col className="ml-auto mr-auto" xs="10">
+            <Card className="card-upgrade" style={{ transform: 'none' }}>
+              <CardHeader className="text-center">
+                <CardTitle tag="h4">Assessment Questions</CardTitle>
+                <p className="card-category">
+                  Assessment Questions for <b>{this.state.currentPolicy.policy_name}</b>
+                </p>
+                <Link to="/dashboard/policies" className="btn btn-danger">
+                  &#8592; Go Back to Policies
+                  </Link>
+              </CardHeader>
+              <CardBody>
+                {this.state.assessment.map((assessment, assessmentIndex) => (
+                  <React.Fragment>
+                    {this.renderAssessment(assessment, assessmentIndex)}
+                  </React.Fragment>
+                ))}
+                <Row style={{ paddingTop: "10px", justifyContent: "center" }}>
+                  <Col xs="10">
+                    <Button className="btn-round" color="success" onClick={this.onSubmit}>Submit</Button>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </>
+    }
+  }
+  // END-Render method
 }
 
 export default Assessment;
